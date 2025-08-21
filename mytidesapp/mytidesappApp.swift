220a1097409d493788f5560f7805544849982d5f
Nick Weiland <nickweiland@gmail.com>
2025-08-21 09:53:50 -0700

    Initial Commit

---
+++ mytidesapp/mytidesappApp.swift
@@ -0,0 +1,17 @@
+//
+//  mytidesappApp.swift
+//  mytidesapp
+//
+//  Created by Nick Weiland on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct mytidesappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mytidesappTests/mytidesappTests.swift
@@ -0,0 +1,17 @@
+//
+//  mytidesappTests.swift
+//  mytidesappTests
+//
+//  Created by Nick Weiland on 8/21/25.
+//
+
+import Testing
+@testable import mytidesapp
+
+struct mytidesappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
